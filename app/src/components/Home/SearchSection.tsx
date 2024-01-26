d4b6a40183f07556c07edcf13d66b22c38aa1138
vaibhav21898 <vj2598054@gmail.com>
2024-01-27 03:11:15 +0530

    add rooms and guest logic done

--- app/src/components/Home/SearchSection.tsx
+++ app/src/components/Home/SearchSection.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { homeHeader } from "../../images";
-import { IoLocateOutline } from "react-icons/io5";
 import { DatePicker, Space } from "antd";
-import { FiMinus } from "react-icons/fi";
 import dayjs from "dayjs";
+import { useState } from "react";
+import { FiMinus } from "react-icons/fi";
+import { GoPlus } from "react-icons/go";
+import { IoLocateOutline } from "react-icons/io5";
+import { homeHeader } from "../../images";
 const SearchSection = () => {
   const [placeholder, setPlaceholder] = useState("Around Me");
   const [rooms, setRooms] = useState<number>(1);
-  const [guest, SetGuest] = useState<number>(1);
   const [roomToggle, setRoomToggle] = useState<boolean>(false);
   const { RangePicker } = DatePicker;
   const currentDate = dayjs();
@@ -17,6 +17,59 @@ const SearchSection = () => {
   };
   const handleBlur = () => {
     setPlaceholder("Around Me");
+  };
+
+  const [roomDetails, setRoomDetails] = useState([
+    {
+      id: 1,
+      guests: 1,
+    },
+  ]);
+
+  const handlePluse = (roomId: number) => {
+    const updatedRooms = roomDetails.map((room) =>
+      room.id === roomId && room.guests < 3
+        ? { ...room, guests: room.guests + 1 }
+        : room
+    );
+    setRoomDetails(updatedRooms as any);
+  };
+
+  const handleMinus = (roomId: number) => {
+    const updatedRooms = roomDetails.map((room) =>
+      room.id === roomId && room.guests > 1
+        ? { ...room, guests: room.guests - 1 }
+        : room
+    );
+    setRoomDetails(updatedRooms);
+  };
+
+  const handleAddRoom = () => {
+    if (rooms < 6) {
+      setRooms((prevRooms) => prevRooms + 1);
+
+      const newRoomId = roomDetails.length + 1;
+      setRoomDetails((prevRooms) => [
+        ...prevRooms,
+        {
+          id: newRoomId,
+          guests: 1,
+        },
+      ]);
+    }
+  };
+
+  const handleDeleteRoom = () => {
+    if (rooms > 1) {
+      setRooms((prevRooms) => prevRooms - 1);
+      const updatedRooms = [...roomDetails];
+      updatedRooms.pop();
+      setRoomDetails(updatedRooms);
+    }
+  };
+
+  const calculateTotalGuests = () => {
+    return roomDetails.reduce((total, room) => total + room.guests, 0);
   };
 
   return (
@@ -29,7 +82,7 @@ const SearchSection = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="grid grid-cols-12  items-center bg-white w-[80%]  ">
+      <div className="  grid grid-cols-12  items-center bg-white w-[80%]  ">
         <div className="relative col-span-4 h-full border-r border-gray-400 ">
           <input
             type="text"
@@ -61,29 +114,54 @@ const SearchSection = () => {
         <div
           onClick={() => setRoomToggle(!roomToggle)}
           // onBlur={() => setRoomToggle(false)}
-          className=" relative col-span-3 cursor-pointer h-full py-5  flex gap-1 justify-center"
+          className="  col-span-3 cursor-pointer h-full py-5  flex gap-1 justify-center"
         >
           <p className="font-bold">{rooms} Room ,</p>
-          <p className="font-bold">{guest} Guest</p>
-          {roomToggle && (
-            <div className="absolute bottom-[-3rem] bg-white rounded-md w-full p-3 shadow-lg border">
-              <div className="flex px-6 justify-between  gap-3 text-sm font-bold">
-                <p>Rooms</p>
-                <p>Guests</p>
-              </div>
-              <div>
-                <p>Room 1</p>
-                <div>
-                  <div>
+          <p className="font-bold">{calculateTotalGuests()} Guest</p>
+        </div>
+        {roomToggle && (
+          <div className="absolute flex flex-col gap-6 top-[45%] right-[23%] bg-white rounded-md w-[20%] p-3 shadow-lg border">
+            <div className="flex px-6 justify-between  gap-3 text-sm font-bold">
+              <p>Rooms</p>
+              <p>Guests</p>
+            </div>
+            {roomDetails.map((room) => (
+              <div key={room.id} className="flex justify-between items-center">
+                <p className="text-sm font-medium">{`Room ${room.id}`}</p>
+                <div className="flex justify-between items-center gap-4">
+                  <div
+                    onClick={() => handleMinus(room.id)}
+                    className="border-2 p-1 text-lg"
+                  >
                     <FiMinus />
                   </div>
-                  <p>{guest}</p>
-                  <div></div>
+                  <p>{room.guests}</p>
+                  <div
+                    onClick={() => handlePluse(room.id)}
+                    className="border-2 p-1 text-lg"
+                  >
+                    <GoPlus />
+                  </div>
                 </div>
               </div>
+            ))}
+
+            <div className="flex justify-between">
+              <button
+                onClick={() => handleDeleteRoom()}
+                className="text-sm bg-red-600 px-3 py-1 text-white font-semibold rounded-sm"
+              >
+                Delete Room
+              </button>
+              <button
+                onClick={() => handleAddRoom()}
+                className="text-sm bg-[#1ab64f] px-3 py-1 text-white font-semibold rounded-sm"
+              >
+                Add Room
+              </button>
             </div>
-          )}
-        </div>
+          </div>
+        )}
 
         <div className="py-5 bg-[#1ab64f] text-lg cursor-pointer font-bold text-white hover:bg-green-700 transition-all duration-300 ease-out col-span-2 h-full text-center ">
           Search
