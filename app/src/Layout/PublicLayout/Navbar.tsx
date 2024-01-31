bd8f6c4a73a46bd1db0028b9270d0a9fe1c73ed9
vaibhav21898 <vj2598054@gmail.com>
2024-01-31 11:09:54 +0530

    working  open nav

--- app/src/Layout/PublicLayout/Navbar.tsx
+++ app/src/Layout/PublicLayout/Navbar.tsx
@@ -15,69 +15,82 @@ const Navbar = () => {
   const handleMenuLeave = () => {
     setHoveredMenu(null);
   };
-  console.log("scrollPosition--", scrollPosition);
 
   return (
-    <section
-      className={`sticky top-0 w-full z-10 border-2 border-red-500 ${
-        scrollPosition > 10 ? "hidden" : "block"
-      } `}
-    >
-      <InfoNav />
-      <div className="bg-[#f3f5f7] w-full ">
-        <div className="flex justify-between public-container  ">
-          {newNavMenues?.map((menu, index: number) => (
-            <div
-              key={menu.id}
-              className="relative group"
-              onMouseEnter={() => handleMenuHover(menu.id)}
-              onMouseLeave={handleMenuLeave}
-            >
-              <Link to={menu.path as any}>
-                <div className="w-full ">
-                  <div
-                    className={`text-sm text-gray-700 flex gap-1 items-center  px-3 py-2 transition-all duration-300 ease-in ${
-                      hoveredMenu === menu.id ? "group-hover:bg-[#fffefe] " : ""
-                    } `}
-                  >
-                    {menu.title}
-                    <HiChevronDown
-                      className={` transition-all duration-300 ease-in-out ${
-                        menu.subMenus ? "block" : "hidden"
-                      } ${
-                        menu.subMenus && hoveredMenu === menu.id
-                          ? "rotate-180"
-                          : "rotate-0"
-                      }`}
-                    />
-                  </div>
-                </div>
-              </Link>
-
-              {menu.subMenus && hoveredMenu === menu.id && (
-                <div
-                  className={`absolute top-full ${
-                    index >= newNavMenues.length - 3 ? "right-0" : "left-0"
-                  } bg-white border shadow-sm rounded-b-lg rounded-tr-lg py-4  pl-6 w-64`}
-                >
-                  <p className="font-bold">Popular Localities</p>
-                  {menu.subMenus.map((subMenu) => (
-                    <Link
-                      key={subMenu.id}
-                      to={subMenu.path as any}
-                      className="block py-1"
+    <section className={`  w-full  `}>
+      <div
+        className={`${
+          scrollPosition < 300 ? "h-0" : "h-20"
+        } fixed bg-white w-full z-10 transition-all duration-300 ease-in-out `}
+      >
+        <div
+          className={`${
+            scrollPosition < 300 ? "hidden" : "block"
+          } transition-all duration-300 ease-in-out`}
+        >
+          <p>1hiudh</p>
+          <p>1hiudh</p>
+        </div>
+      </div>
+      <div className={`sticky bg-white z-10`}>
+        <InfoNav />
+        <div className="bg-[#f3f5f7] w-full ">
+          <div className="flex justify-between public-container  ">
+            {newNavMenues?.map((menu, index: number) => (
+              <div
+                key={menu.id}
+                className="relative group"
+                onMouseEnter={() => handleMenuHover(menu.id)}
+                onMouseLeave={handleMenuLeave}
+              >
+                <Link to={menu.path as any}>
+                  <div className="w-full ">
+                    <div
+                      className={`text-sm text-gray-700 flex gap-1 items-center  px-3 py-2 transition-all duration-300 ease-in ${
+                        hoveredMenu === menu.id
+                          ? "group-hover:bg-[#fffefe] "
+                          : ""
+                      } `}
                     >
-                      <p className="hover:text-mainColor"> {subMenu.title}</p>
-                    </Link>
-                  ))}
-                  <div className="flex gap-2 items-center text-mainColor">
-                    <p> All of {menu.title} </p>
-                    <IoIosArrowForward />
+                      {menu.title}
+                      <HiChevronDown
+                        className={` transition-all duration-300 ease-in-out ${
+                          menu.subMenus ? "block" : "hidden"
+                        } ${
+                          menu.subMenus && hoveredMenu === menu.id
+                            ? "rotate-180"
+                            : "rotate-0"
+                        }`}
+                      />
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
+                </Link>
+
+                {menu.subMenus && hoveredMenu === menu.id && (
+                  <div
+                    className={`absolute top-full ${
+                      index >= newNavMenues.length - 3 ? "right-0" : "left-0"
+                    } bg-white border shadow-sm rounded-b-lg rounded-tr-lg py-4  pl-6 w-64`}
+                  >
+                    <p className="font-bold">Popular Localities</p>
+                    {menu.subMenus.map((subMenu) => (
+                      <Link
+                        key={subMenu.id}
+                        to={subMenu.path as any}
+                        className="block py-1"
+                      >
+                        <p className="hover:text-mainColor"> {subMenu.title}</p>
+                      </Link>
+                    ))}
+                    <div className="flex gap-2 items-center text-mainColor">
+                      <p> All of {menu.title} </p>
+                      <IoIosArrowForward />
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
